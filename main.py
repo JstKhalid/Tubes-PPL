4ac9fcb41e58de5bdb76ecf8e5e0dea507f28248
KhalidAsyst <khalid.saifullah@asyst.co.id>
2023-12-19 15:49:45 +0700

    slight changes

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ while(True):
     print("="*50)
     print("AKSES APLIKASI")
     try:
-        print("1. Admin \n2. Customer \n3. Keluar ")
+        print("1. Admin \n2. Customer \n3. Keluar \n")
         akses = int(input("pilih akses anda: "))
         if akses == 1:
             print("="*50)
@@ -18,7 +18,7 @@ while(True):
             while(True):
                 print("===== MENU ADMIN =====")
                 try:
-                    print("1. Tampilkan Semua Data \n2. Tambah Data \n3. Ubah Data \n4. Hapus Data \n 5.Keluar Menu Admin \n")
+                    print("1. Tampilkan Semua Data \n2. Tambah Data \n3. Ubah Data \n4. Hapus Data \n5.Keluar Menu Admin \n")
                     pilih_admin = int(input("pilih menu: "))
                     if pilih_admin == 1:
                         read_items(database)
