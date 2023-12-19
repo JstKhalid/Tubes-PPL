9393ae7827ae52be8ec7e442619642ee18dc4b7f
Wahyuy21 <wahyu.pratama@asyst.co.id>
2023-12-20 05:17:39 +0700

    Add type into database, Add 2 Cust Menus (add and checkout)

--- Controller.py
+++ Controller.py
@@ -1,12 +1,12 @@
 #FUNCTION CRUD ADMIN
 def create_item_makanan( makanan, harga, stock, database):
     new_id = max(item['id'] for item in database) + 1 if database else 1
-    new_item = {"id": new_id, "makanan": makanan, "harga": harga, "stock": stock}
+    new_item = {"id": new_id, "type":"makanan","nama": makanan, "harga": harga, "stock": stock}
     database.append(new_item)
 
 def create_item_minuman( minuman, harga, stock, database):
     new_id = max(item['id'] for item in database) + 1 if database else 1
-    new_item = {"id": new_id, "minuman": minuman, "harga": harga, "stock": stock}
+    new_item = {"id": new_id,"type":"minumam","nama": minuman, "harga": harga, "stock": stock}
     database.append(new_item)
 
   
@@ -29,9 +29,45 @@ def delete_item(item_id, database):
             return "Item deleted successfully"
     return "Item not found"
 
-def show_data(db_makanan, db_minuman):
-    print("===== DATA MAKANAN =====")
-    read_items(db_makanan)
-    print("===== DATA MINUMAN =====")
-    read_items(db_minuman)
+def show_data(db_makanan, db_minuman,item_type=0):
 
+    if item_type == 1:
+        print("\n===== MAKANAN =====")
+        for row in db_makanan:
+            print(f"{row['id']}. {row['nama']} | Harga : Rp {row['harga']},00 | Stok : {row['stock']}")
+    elif item_type == 2:
+        print(" \n===== MINUMAN =====")
+        for row in db_minuman:
+            print(f"{row['id']}. {row['nama']} | Harga : Rp {row['harga']},00 | Stok : {row['stock']}")
+    elif item_type == 0:
+        print("\n===== MAKANAN =====")
+        for row in db_makanan:
+            print(f"{row['id']}. {row['nama']} | Harga : Rp {row['harga']},00 | Stok : {row['stock']}")
+        print(" \n===== MINUMAN =====")
+        for row in db_minuman:
+            print(f"{row['id']}. {row['nama']} | Harga : Rp {row['harga']},00 | Stok : {row['stock']}")
+
+def add_item (db_makanan, db_minuman,item_type,cart):
+        try:
+            show_data(db_makanan,db_minuman,item_type)
+
+            if item_type == 1:
+                type = "makanan"
+                database = db_makanan
+            elif item_type == 2:
+                type = "minuman"
+                database = db_minuman
+            item_no = int(input(f"Pilih {type} (Nomor):"))
+            selected_item = [item for item in database if item['id'] == item_no][0]
+            item_id =  selected_item['id'] 
+
+            item_name = selected_item['nama']
+            item_price = selected_item['harga']
+            item_stock = selected_item['stock']
+            jumlah_item = int(input("Masukkan Jumlah : "))
+            if jumlah_item > item_stock :
+                print("\nGagal Menambahkan (Stok Habis)")
+            cart_item = {"item_no":item_id,"type":type,"nama":item_name,"harga":item_price,"jumlah":jumlah_item}
+            cart.append(cart_item)
+        except IndexError:
+            print("\nItem Tidak Terdaftar")  
--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from Controller import *
 
 database_makanan = [
-    {"id": 1, "makanan": "Nasi Goreng", "harga": 25000, "stock": 10},
-    {"id": 2, "makanan": "Mie Ayam",  "harga": 20000, "stock": 15},
+    {"id": 1, "type" :"makanan", "nama": "Nasi Goreng", "harga": 25000, "stock": 10},
+    {"id": 2, "type" :"makanan", "nama": "Mie Ayam",  "harga": 20000, "stock": 15},
 ]
 
 database_minuman = [
-    {"id": 1, "minuman": "Teh Manis", "harga": 25000, "stock": 10},
-    {"id": 2, "minuman": "Es Jeruk", "harga": 20000, "stock": 15},
+    {"id": 1, "type" :"minuman", "nama": "Teh Manis", "harga": 25000, "stock": 10},
+    {"id": 2, "type" :"minuman","nama": "Es Jeruk", "harga": 20000, "stock": 15},
 ]
 
 
-print("===== SELAMAT DATANG DI FOOD ORDERING APP =====")
+
+
+print("\n===== SELAMAT DATANG DI FOOD ORDERING APP =====")
 while True:
     print("="*50)
     print("AKSES APLIKASI")
@@ -22,17 +24,20 @@ while True:
             print("="*50)
             print("Signed in as Admin")
             while True:
-                print("===== MENU ADMIN =====")
+                print("\n===== MENU ADMIN =====")
                 try:
                     print("1. Tampilkan Semua Data \n2. Tambah Data \n3. Ubah Data \n4. Hapus Data \n5. Keluar Menu Admin \n")
                     pilih_admin = int(input("pilih menu: "))
                     if pilih_admin == 1:
+                        # print("1. Makanan \n2. Minuman ")
+                        # item_type = int(input("Masukkan Jenis Item"))
+                        # show_data(database_makanan,database_minuman,item_type)
                         show_data(database_makanan,database_minuman)
                     elif pilih_admin == 2:
                         while True:
                             try:
                                 print("PILIH DATA YANG INGIN DI BUAT")
-                                print("1. Data Makanan \n2. Data Minuman \n3. kembali")
+                                print("1. Data Makanan \n2. Data Minuman \n 3. Kembali")
                                 pilih_data = int(input("Pilih no (1 - 3): "))
                                 if pilih_data == 1:
                                     makanan = input("Makanan    : ")
@@ -46,10 +51,10 @@ while True:
                                     stock = input("stock        : ")
                                     create_item_minuman(minuman, harga, stock, database_minuman)
                                     print("data berhasil dibuat")
-                                elif pilih_data == 3:
+                                elif pilih_data == 4:
                                     break
                                 else:
-                                    print("Pilih Nomor 1 - 3")
+                                    print("Pilih Nomor 1 - 4")
                                     False
                             except ValueError:
                                 print("Program Hanya Memproses inputan angka, silahkan pilih lagi (1 - 4)")
@@ -118,7 +123,46 @@ while True:
                 except ValueError:
                     print("Program Hanya Memproses inputan angka, silahkan pilih lagi (1 - 4)")
         elif akses == 2:
-            print("Masuk as customer")
+            print("="*50)
+            print("Selamat Datang Customer")
+            cart = []
+            total = 0
+            while True:
+                print("\n===== MENU =====")
+                try:
+                    print("Kerangjang Anda : ")
+                    for row in cart:
+                        print(row)
+                    print("\nAksi : \n1. Beli Item \n2. Hapus Item \n3. Ubah Item \n4. Checkout \n6. Keluar \n")
+                    cust_menu = int(input("Pilih Menu (1-6) : "))
+                    if cust_menu == 1:
+                        print("\nAksi : \n1. Makanan \n2. Minuman \n3. Keluar \n")
+                        cust_menu = int(input("Pilih Menu (1-3) : "))
+                        if cust_menu == 1: 
+                            add_item(database_makanan,database_minuman,cust_menu,cart)
+                                
+                        elif cust_menu == 2:
+                            add_item(database_makanan,database_minuman,cust_menu,cart)
+                        elif cust_menu == 3:
+                            continue
+                        
+                    elif cust_menu == 2:
+                        pass
+                    elif cust_menu == 3:
+                        pass
+                    elif cust_menu == 4:
+                        if len(cart)<1:
+                            print("Anda Belum Menambahkan Item")
+                            continue
+                        for row in cart:
+                            total += row['harga']*row['jumlah']
+                        print(f"Total Harga = Rp {total},00")
+                        print("TERIMA KASIH SUDAH MEMESAN")
+                        break
+                    elif cust_menu == 5:
+                        break
+                except ValueError:
+                    print("Program Hanya Memproses inputan angka, silahkan pilih lagi (1 - 6)")
         elif akses == 3:
             print("TERIMA KASIH SUDAH MENGGUNAKAN FOOD ORDERING APP")
             break
